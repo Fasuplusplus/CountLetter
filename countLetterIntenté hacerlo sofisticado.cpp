d5be3c4c5ecf5c05171a1c1150ee553e745362db
Fasuplusplus <facundo_forte@hotmail.com>
2021-06-08 10:12:43 -0300

    todo terminado

---
+++ countLetterIntenté hacerlo sofisticado.cpp
@@ -0,0 +1,49 @@
+//Introducir una frase y después una letra, cuyas apariciones en la frase se contarán
+//ESTO NO FUNCIONA PARA NADA
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+string getPhrase() {
+    string input;
+    cout<<"Introduzca una frase"<<"\n";
+    getline(cin, input);
+    return input;
+}
+string getLetter() {
+    string input;
+    cout<<"Introduzca una letra para contar"<<"\n";
+    check:
+    getline(cin, input);
+    if ((input.length()) == 1) {
+        return input;
+    }
+    else {
+        cout<<"Introduzca solo un caracter"<<"\n";
+        goto check;
+    }   
+}
+bool isSame(int e) {
+    if((getPhrase()[e]) == (getLetter()[0])) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+string countLetter() {  //acá se podía usar un range-defined loop pero no entiendo la documentación
+    int count = 0;
+    for(int i = 0; i < (getPhrase().length()); i++) {
+        if (isSame(i) == true) {
+            count++;
+        }
+    }
+    stringstream preoutput;
+    preoutput <<"La letra "<< getLetter() <<" se repite "<< count << " veces en esta frase" << "\n";
+    string output = (preoutput.str());
+    return output;
+}
+int main() {
+    printf("%s\n", countLetter()); //JAJAJAJAJAJAJJAJAJAJAJAJAJASJAJSAJSAJDFsfrhgiljmergikrjm fffffffffnrfgjmnernergikerjgmeriogjmdflkgdgrjgdkftjgshbdfmgeribmrirmimrimi
+    return 0;
+}
